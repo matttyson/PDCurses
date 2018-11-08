8f9bb0bee4779410ff9d1378ca4474a867de31dc
Matt Tyson <mtyson@mtyson.id.au>
2018-11-08 16:33:21 +1000

    More keyboard codes.
    Still more to do here.

--- direct2d/pdc_d2d_util.c
+++ direct2d/pdc_d2d_util.c
@@ -66,7 +66,6 @@ static chtype _process_key_event_2(WPARAM vk, LPARAM state)
 {
     pdc_key_modifiers = 0L;
 
-
     if (SP->save_key_modifiers){
         if(GetKeyState(VK_SHIFT) & 0x8000){
             pdc_key_modifiers |= PDC_KEY_MODIFIER_SHIFT;
@@ -79,31 +78,111 @@ static chtype _process_key_event_2(WPARAM vk, LPARAM state)
         }
     }
     else if(SP->return_key_modifiers){
-
-        //return -1;
+        SP->key_code = TRUE;
+        if(GetKeyState(VK_RSHIFT) & 0x8000){
+            return KEY_SHIFT_R;
+        }
+        else if(GetKeyState(VK_LSHIFT)){
+            return KEY_SHIFT_L;
+        }
+        else if(GetKeyState(VK_LCONTROL) & 0x8000){
+            return KEY_CONTROL_R;
+        }
+        else if(GetKeyState(VK_RCONTROL) & 0x8000){
+            return KEY_CONTROL_L;
+        }
+        else if(GetKeyState(VK_RMENU) & 0x8000){
+            return KEY_ALT_R;
+        }
+        else if(GetKeyState(VK_LMENU) & 0x8000){
+            return KEY_ALT_L;
+        }
+        SP->key_code = FALSE;
     }
-
-    SP->key_code = TRUE; // ?
 
     // 0 - 9
     if(vk >= 0x30 && vk <= 0x39){
+        if (state & (1 << 29)) { // ALT
+            switch(vk){
+            case '0': return ALT_0;
+            case '1': return ALT_1;
+            case '2': return ALT_2;
+            case '3': return ALT_3;
+            case '4': return ALT_4;
+            case '5': return ALT_5;
+            case '6': return ALT_6;
+            case '7': return ALT_7;
+            case '8': return ALT_8;
+            case '9': return ALT_9;
+            }
+        }
         return vk;
     }
     // A - Z
     if(vk >= 0x41 && vk <= 0x5A){
-        return vk;
+        if(state & (1 << 29)){ // ALT
+            switch(vk){
+            case 'A': return ALT_A;
+            case 'B': return ALT_B;
+            case 'C': return ALT_C;
+            case 'D': return ALT_D;
+            case 'E': return ALT_E;
+            case 'F': return ALT_F;
+            case 'G': return ALT_G;
+            case 'H': return ALT_H;
+            case 'I': return ALT_I;
+            case 'J': return ALT_J;
+            case 'K': return ALT_K;
+            case 'L': return ALT_L;
+            case 'M': return ALT_M;
+            case 'N': return ALT_N;
+            case 'O': return ALT_O;
+            case 'P': return ALT_P;
+            case 'Q': return ALT_Q;
+            case 'R': return ALT_R;
+            case 'S': return ALT_S;
+            case 'T': return ALT_T;
+            case 'U': return ALT_U;
+            case 'V': return ALT_V;
+            case 'W': return ALT_W;
+            case 'X': return ALT_X;
+            case 'Y': return ALT_Y;
+            case 'Z': return ALT_Z;
+            }
+        }
+
+        if (GetKeyState(VK_CAPITAL) & 0x0001) {
+            return vk;
+        }
+        if(GetKeyState(VK_SHIFT) & 0x8000){
+            return vk;
+        }
+
+        // lowercase
+        return vk + 0x20;
+    }
+
+    // F1 - F24 function keys
+    if(vk >= 0x70 && vk <= 0x87){
+        return KEY_F((vk - 0x70 +1));
     }
 
     switch(vk){
-    case VK_BACK: return KEY_BACKSPACE;
-//    case VK_TAB: return KEY_TAB
     case VK_LEFT: return KEY_LEFT;
     case VK_RIGHT: return KEY_RIGHT;
     case VK_UP: return KEY_UP;
     case VK_DOWN: return KEY_DOWN;
     case VK_INSERT: return KEY_IC;
+    case VK_DELETE: return KEY_DC;
+    case VK_HOME: return KEY_HOME;
+    case VK_END: return KEY_END;
+    case VK_PRIOR: return KEY_PPAGE;
+    case VK_NEXT: return KEY_NPAGE;
     case VK_ESCAPE: return 0x1B;
     case VK_RETURN: return '\n';
+    case VK_TAB: return '\t';
+    case VK_BACK: return 0x08;
+    case VK_SPACE: return ' ';
     }
 
 
